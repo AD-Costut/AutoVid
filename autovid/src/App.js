be16be07e610e0dec01e2d14f49a99a7c09b03d2
AD-Costut <costut_ad05@yahoo.com>
2025-03-26 20:50:46 +0200

    Created Navbar

--- autovid/src/App.js
+++ autovid/src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import Navbar from "./Navbar/Navbar";
+import ContactUs from "./pages/ContactUs";
+import About from "./pages/About";
+import Login from "./pages/Login";
+import Services from "./pages/Services";
+import Home from "./pages/Home";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {" "}
+      <Navbar />
+      <div className="container">
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/about" element={<About />}></Route>
+          <Route path="/services" element={<Services />}></Route>
+          <Route path="/contact-us" element={<ContactUs />}></Route>
+          <Route path="/login" element={<Login />}></Route>
+        </Routes>
+      </div>
+    </>
   );
 }
 
